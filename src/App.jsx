49d921dbdbb6d9ac6ceba6009d5ee6c05ba75186
KrissAsterisk <crisrogamer@gmail.com>
2026-03-26 12:55:49 +0200

    went over what is JSX (used to display to UI, transpiled to JS with Babel, allows us to insert vars directly to UI with {}

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import './App.css'
 
-const TITLE = {
-    name: "reactz"
+const title = {
+    name: "Reactz",
+    introduction: "Presents",
+    body: "Welcome to my page!"
 };
 
-let makeNoise = () => {
-    let noiseToMake = "AAAAAAAAAAAAAAAH";
-    return noiseToMake;
-}
+
+
+let jsArray = [`el1`, `el2`, `el3`];
+let makeNoise = () => "AAAAAAAAAAAAAAAAH";
 function App() {
 
     console.log(makeNoise());
 
+
     return (
         <div>
-            <h1>AAAAAAAAAAAAAAAAH</h1>
-        </div>
+            <h1>{makeNoise()}</h1>
+            <h1>{title.name} {title.introduction}:</h1>
+            <h1>{title.body}</h1>
+            <h1>
+                {
+                    jsArray.map((value) => value.replace(value.charAt(value.length-1), "X"))
+
+                }
+            </h1>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
+        </div>z
+
     )
 }
 
