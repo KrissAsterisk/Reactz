5d8bb3a136b181d331fdce8fcb745f104b7ef04c
KrissAsterisk <crisrogamer@gmail.com>
2026-03-26 15:21:42 +0200

    read about JSX expr, lists in JSX, i prefer explicit lamba calls cause they look cool & shorter to write (also make more sense in my head)

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,87 @@
-import './App.css'
-
 const title = {
     name: "Reactz",
     introduction: "Presents",
     body: "Welcome to my page!"
 };
 
+const list = [{
+    title: "Reactz",
+    url: "localhost:9090",
+    author: "me",
+    num_comments: 3,
+    points: 4,
+    gayness: 100,
+    objectId: 0,
+},
+{
+    title: "AuthServer",
+    url: "authserver:9000",
+    author: "admin",
+    num_comments: 0,
+    points: 999,
+    objectId: 1,
+},
+{
+    title: "AnotherAdition",
+    url: "0.0.0.0:0000",
+    author: "TBD",
+    num_comments: null,
+    points: null,
+    objectId: 2
+
+}];
+
 
 
 let jsArray = [`el1`, `el2`, `el3`];
 let makeNoise = () => "AAAAAAAAAAAAAAAAH";
+title.defaultSound = makeNoise();
 function App() {
 
     console.log(makeNoise());
 
-
     return (
         <div>
-            <h1>{makeNoise()}</h1>
             <h1>{title.name} {title.introduction}:</h1>
             <h1>{title.body}</h1>
             <h1>
                 {
-                    jsArray.map((value) => value.replace(value.charAt(value.length-1), "X"))
+                    jsArray.map((item) => item.replace(item.charAt(item.length - 1), "X"))
 
                 }
             </h1>
+            <ul>
+                {
+                    list.map((item) => {
+
+                        let delistedItems = [];
+
+                        for (let key in item) {
+                            switch (key) {
+                                case "url": {
+                                    var linkToUrl = <a href={`http://${item[key]}`}>{item.title}:</a>;
+                                    delistedItems.push(linkToUrl);
+                                    break;
+                                }
+                                case "objectId": break;
+                                case "title": break;
+                                default: delistedItems.push(<ul><li>{`${key}: ${item[key]} \n`}</li></ul>)
+                            }
+                        }
+
+                        return <li key={item.objectId}>
+
+                            {delistedItems}
+
+                        </li>
+
+                    })
+
+                }
+            </ul>
             <label htmlFor="search">Search: </label>
             <input id="search" type="text" />
-        </div>z
+        </div>
 
     )
 }
